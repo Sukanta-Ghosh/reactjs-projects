3e8d3e9e4d29ecf491b9ec066ad9bc3e42239238
Sukanta Ghosh <48876856+Sukanta-Ghosh@users.noreply.github.com>
2025-05-03 23:21:04 +0530

    Push

--- system-designs/toast-component/src/App.jsx
+++ system-designs/toast-component/src/App.jsx
@@ -1,15 +1,50 @@
 import useNotification from "./ts-components/useNotification";
 import "./App.css";
 import "./Notification.css";
+import { useState } from "react";
+
+const configPositions = [
+  "top-left",
+  "top-right",
+  "bottom-left",
+  "bottom-right",
+];
+
+const configAnimations = ["slide", "fade", "pop"];
 
 function App() {
+  // states
+  const [position, setPosition] = useState(configPositions[0]);
+  const [animation, setAnimation] = useState(configAnimations[0]);
+
+  // hooks
   const { NotificationComponent, triggerNotification } =
-    useNotification("top-left");
+    useNotification(position);
 
   return (
     <div className="App">
       {NotificationComponent}
       <h1>Toast Component</h1>
+      <label className="configs">
+        Positions: &nbsp;
+        <select onChange={(e) => setPosition(e.target.value)}>
+          {configPositions.map((position, idx) => (
+            <option key={idx} value={position}>
+              {position}
+            </option>
+          ))}
+        </select>
+      </label>
+      <label className="configs">
+        Animations: &nbsp;
+        <select onChange={(e) => setAnimation(e.target.value)}>
+          {configAnimations.map((animation, idx) => (
+            <option key={idx} value={animation}>
+              {animation}
+            </option>
+          ))}
+        </select>
+      </label>
       <div className="btns">
         <button
           onClick={() =>
@@ -17,7 +52,7 @@ function App() {
               type: "success",
               message: "This is a success message!",
               duration: 3000,
-              animation: "pop",
+              animation: animation,
             })
           }
         >
@@ -29,6 +64,7 @@ function App() {
               type: "info",
               message: "This is an info message!",
               duration: 3000,
+              animation: animation,
             })
           }
         >
@@ -40,6 +76,7 @@ function App() {
               type: "warning",
               message: "This is a warning message!",
               duration: 3000,
+              animation: animation,
             })
           }
         >
@@ -51,6 +88,7 @@ function App() {
               type: "error",
               message: "This is an error message!",
               duration: 3000,
+              animation: animation,
             })
           }
         >
